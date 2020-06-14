9c9dbc57b1f17e127715ac47dc70f4a12eb24bfa
Suresh Maidaragi <maidaragi1919@gmail.com>
2020-06-14 08:46:12 +0530

    Initial setup

---
+++ AndroidStudioProject/app/src/main/java/com/problems/alogrithms/SolveMeFirst.kt
@@ -0,0 +1,40 @@
+package com.problems.alogrithms
+
+import java.util.*
+
+/*
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int a, int b);
+
+where,
+
+a is the first integer input.
+b is the second integer input
+Return values
+
+sum of the above two integers
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+
+The sum of the two integers  and  is computed as: .*/
+
+fun solveMeFirst(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+    val sum = solveMeFirst(num1, num2)
+    println(sum)
+}
